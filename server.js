f613c7a246f7d06142c3b6a800cf681817e3edbe
pierlag <pierlag@microsoft.com>
2015-03-09 13:35:30 -0700

    v2

--- server.js
+++ server.js
@@ -4,4 +4,3 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
-//# sourceMappingURL=server.js.map
